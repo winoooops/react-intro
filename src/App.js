63affbac980ed108fefb00a51ca0a32ac2f3e706
weiwang <--add>
2019-07-31 12:11:38 +1000

    change AppCom into es6 class

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Welcome from './components/welcome';
 import List from './components/list'
 import TodoForm from './components/todo-form'
 
-function App() {
-  return (
-    <div className="App">
-      <Welcome name="Will"></Welcome>
-      <List></List>
-      <TodoForm></TodoForm>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Welcome name="Will"></Welcome>
+        <List></List>
+        <TodoForm></TodoForm>
+      </div>
+    );
+  }
 }
 
 export default App;
